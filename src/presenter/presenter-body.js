e97f4f32731d34dfab177b40239f551658fd06c8
Dmitriy Borovkov <borovvkovv@gmail.comi>
2022-09-22 19:38:35 +0500

    Исправил отображение popup

--- src/presenter/presenter-body.js
+++ src/presenter/presenter-body.js
@@ -5,7 +5,8 @@ import PopupView from '../view/popup-view.js';
 export default class PresenterBody {
   init = (container) => {
     this.container = container;
-    render(new PopupView(), container, RenderPosition.AFTEREND);
+    document.body.classList.add('hide-overflow');
+    render(new PopupView(), this.container, RenderPosition.AFTEREND);
 
   };
 }
